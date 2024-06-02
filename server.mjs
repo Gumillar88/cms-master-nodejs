a07bd47f7b528194a52fff84fa6b60b7613c592c
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-06-02 23:01:10 +0700

    Update Server.mjs

--- server.mjs
+++ server.mjs
@@ -27,6 +27,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the "public" directory
+app.use(express.static(join(__dirname, 'public')));
+
 /*
 ** Session configuration
 ** Use SESSION_SECRET from .env
