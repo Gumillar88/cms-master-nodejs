5b01cdc5cb9d210f73fad94809da7345ed64913e
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-06-03 17:17:46 +0700

    Controllers: Update authController.mjs

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -1,11 +1,32 @@
 // controllers/authController.mjs
-import {renderLoginPage} from '../views/login.mjs';
+import { renderLoginPage } from '../views/login.mjs';
+import { authenticateUser } from '../models/authModel.mjs';
+
 
 const loginRender = (req, res) => {
-    const pageContent = renderLoginPage();
+    const csrfToken = req.csrfToken();
+  
+    const pageContent = renderLoginPage(csrfToken);
     res.send(pageContent);
 };
 
+export const loginHandle = async (req, res) => {
+    const { email, password } = req.body;
+    // console.log(req.body);
+    try {
+        const user = await authenticateUser(email, password);
+      
+        if (user) {
+            req.session.user = user;
+            res.status(200).json({ message: 'Login successful', redirectUrl: '/thor' });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
 export {
-  loginRender,
+  loginRender
 };
