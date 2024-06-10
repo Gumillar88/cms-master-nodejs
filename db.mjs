ddb13ac0554287a1d2f150f149b90c6792c6ed04
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-06-10 18:41:57 +0700

    Update DB Modules Javascript

--- db.mjs
+++ db.mjs
@@ -5,10 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+	host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 export default connection;
